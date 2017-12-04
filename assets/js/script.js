3f4c1b303adf5abef4446151bd6a78b1bb54cfd3
thisisgene <emdo2000@gmail.com>
2017-12-04 15:51:58 +0100

    Changed video directory path.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function getRdmVid(obj) {
   var rdmNumber = Math.floor((Math.random() * 3) + 1);
 
   console.log(rdmNumber);
-  var source = '../assets/img/videos/screen_' + rdmNumber + '.mov';
+  var source = 'assets/img/videos/screen_' + rdmNumber + '.mov';
   var $video = $('#video-background');
   var $source = $video.find('#video-src');
   $source.attr('src', source);
